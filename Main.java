0f1a00fc11dd030abc8abb0ef1970d3cda5300f1
LoraCutelesse <156152402+LoraCuteless@users.noreply.github.com>
2024-05-01 17:33:59 +0800

    Updated

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Text
         System.out.println("Hey its Working!");
         System.out.print("Amazing!"); //Print text with next line automatically
-        System.out.print("Its Working!"); // Print text normally no next line
+        System.out.println("Its Amazing!"); 
+
+        //Number
+        System.out.println(2 + 3); // Print mathematical
+        System.out.println(20); // Print number
+
+        //with Variable Tect
+        String name = "Joy";
+        System.out.println(name);
+
+        int myNum = 15;
+        myNum = 20;  // myNum is now 20
+        System.out.println(myNum);
     }
 }
