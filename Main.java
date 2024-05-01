31fc608dae381568808af4a8c04ecd6ab039cf98
LoraCutelesse <156152402+LoraCuteless@users.noreply.github.com>
2024-05-01 17:21:34 +0800

    Start Up

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey its Working!");
+        System.out.print("Amazing!"); //Print text with next line automatically
+        System.out.print("Its Working!"); // Print text normally no next line
+    }
+}
